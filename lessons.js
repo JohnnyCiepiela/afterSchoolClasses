de5c083771b7c457820175e5e4f844e0ab4fee5e
JohnnyCiepiela <99767967+JohnnyCiepiela@users.noreply.github.com>
2022-11-09 11:01:05 +0100

    Refactoring the project, adding lessons.js file.

---
+++ lessons.js
@@ -0,0 +1,147 @@
+let lessons = [
+
+    {
+        "id": 1001,
+        "title": "Math",
+        "location": "Hendon",
+        "description": "Fundamentals of math",
+        "price": 100,
+        "image": "/images/math_icon.png",
+        "availableSpaces":5,
+        "rating": 2
+     },
+
+     {
+        "id": 1002,
+        "title": "Spanish",
+        "location": "Colindale",
+        "description": "Learn Spanish!",
+        "price": 150,
+        "image": "/images/spanish_icon.png",
+        "availableSpaces":5,
+        "rating": 3
+        
+     },
+
+     {
+        "id": 1003,
+        "title": "Cooking",
+        "location": "Harrow",
+        "description": "Cooking essentials!",
+        "price": 200,
+        "image": "/images/cooking_icon.png",
+        "availableSpaces":5,
+        "rating": 4
+        
+     },
+
+     {
+        "id": 1004,
+        "title": "Arts & Crafts",
+        "location": "Wembley",
+        "description": "Express yourself at arts and crafts!",
+        "price": 175,
+        "image": "/images/art_icon.png",
+        "availableSpaces":5,
+        "rating": 5
+        
+     },
+
+     {
+        "id": 1005,
+        "title": "Biology",
+        "location": "Statford",
+        "description": "Biology description!",
+        "price": 140,
+        "image": "/images/biology_icon.png",
+        "availableSpaces":5,
+        "rating": 1
+        
+     },
+
+     {
+        "id": 1006,
+        "title": "Chemistry",
+        "location": "Kensington",
+        "description": "Chemistry description!",
+        "price": 130,
+        "image": "/images/chemistry_icon.png",
+        "availableSpaces":5,
+        "rating": 2
+        
+     },
+
+     {
+        "id": 1007,
+        "title": "Geography",
+        "location": "South Kensington",
+        "description": "Geography description!",
+        "price": 120,
+        "image": "/images/geography_icon.png",
+        "availableSpaces":5,
+        "rating": 3
+        
+     },
+
+     {
+        "id": 1008,
+        "title": "History",
+        "location": "Greenwich",
+        "description": "History description!",
+        "price": 100,
+        "image": "/images/history_icon.png",
+        "availableSpaces":5,
+        "rating": 1
+        
+     },
+
+     {
+        "id": 1009,
+        "title": "Photography",
+        "location": "Camden",
+        "description": "Photography description!",
+        "price": 165,
+        "image": "/images/photography_icon.png",
+        "availableSpaces":5,
+        "rating": 5
+        
+     },
+
+     {
+        "id": 1010,
+        "title": "Physics",
+        "location": "Hackney",
+        "description": "Physics description!",
+        "price": 170,
+        "image": "/images/physics_icon.png",
+        "availableSpaces":5,
+        "rating": 1
+        
+     },
+
+     {
+        "id": 1011,
+        "title": "Business",
+        "location": "Islington",
+        "description": "Business description!",
+        "price": 200,
+        "image": "/images/business_icon.png",
+        "availableSpaces":5,
+        "rating": 3
+        
+     },
+
+     {
+        "id": 1012,
+        "title": "Zoology",
+        "location": "Hounslow",
+        "description": "Zoology description!",
+        "price": 125,
+        "image": "/images/zoology_icon.png",
+        "availableSpaces":5,
+        "rating": 2
+        
+     },
+
+
+]
